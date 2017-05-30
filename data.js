69cae5bf507d4e6ad4efeafafe8f91c7f1fbcc8d
snaipi91 <snaipi91@rambler.ru>
2017-05-30 19:02:12 +0400

    add file data.js (store)

---
+++ data.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        "cityFrom": "Воронеж",
+        "cityTo": "Москва",
+        "dateTimeFrom": 1496120400,
+        "dateTimeTo": 1496152800,
+        "price": 500
+    },
+    {
+        "cityFrom": "Воронеж",
+        "cityTo": "Москва",
+        "dateTimeFrom": 1496124000,
+        "dateTimeTo": 1496145600,
+        "price": 600
+    },
+    {
+        "cityFrom": "Воронеж",
+        "cityTo": "Москва",
+        "dateTimeFrom": 1496116800,
+        "dateTimeTo": 1496156400,
+        "price": 700
+    }
+]
